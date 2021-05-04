e0373f48757bf3e7a5bbb34b62fc97ff4775e950
raphaelpalhano <raphael1angel@hotmail.com>
2021-05-04 17:17:35 -0300

    classes

---
+++ src/PROVA/musica.java
@@ -0,0 +1,24 @@
+public class Musica {
+    //DECLARAÇÃO DE ATRIBUTOS
+    private String nomeMusica;
+    private String nomeInterprete;
+    private String  nomeCompositor;
+    private int anoComposicaoMusica;
+
+     
+    public Musica(){
+        // INICIANDO MÉTODO CONSTRUTOR
+        nomeMusica = "";
+        nomeInterprete = "";
+        nomeCompositor = "";
+        anoComposicaoMusica = 0;
+    }
+
+    public Musica(String smusica, String sinterprete, String scompositor, int anocomposicao){
+        nomeMusica = smusica;
+        nomeInterprete = sinterprete
+        
+
+    }
+
+}
---
+++ src/classEobjetos/swith.java
@@ -0,0 +1,15 @@
+public class swith {
+    public static void main(String[] args){
+        int value;
+        value = 1;
+        switch (value) {
+            case value:
+                
+                break;
+        
+            default:
+                break;
+        }
+    }
+    
+}
